93f05a773cd1bea394e5d03d49e5123e7b4bd5a0
ashwani1218 <ashwanipandey1218@gmail.com>
2021-10-13 17:24:52 +0530

    Initial commit

---
+++ src/main/java/com/ashwani/testcontainer/TestContainerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ashwani.testcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainerExampleApplication.class, args);
+	}
+
+}
